b2cc46dfaff592a3bb5a6953effca5258586295e
Ling <1427917847@qq.com>
2025-05-13 15:53:27 +0800

    移除异常处理

--- lib/basic_http_manager.dart
+++ lib/basic_http_manager.dart
@@ -2,6 +2,4 @@
 library;
 
 export 'src/basic_http_manager.dart';
-export 'src/models/http_exception.dart';
 export 'src/models/http_manager_config.dart';
-export 'src/models/http_response.dart';
--- test/src/basic_http_manager_test.dart
+++ test/src/basic_http_manager_test.dart
@@ -3,20 +3,22 @@
 import 'dart:convert';
 
 import 'package:basic_http_manager/basic_http_manager.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Basic Restful Api Test', () {
     final basicHttpManager = BasicHttpManager(
-      managerConfig: HttpManagerConfig(baseUrl: 'https://api.askpic.com/',interceptors: []),
+      managerConfig: HttpManagerConfig(
+          baseUrl: 'https://api.askpic.com/', interceptors: []),
     );
-    
+
     test('Get', () async {
-      
       final resp = await basicHttpManager.get('/conversations');
-      print('${'=' * 10}\n${resp.isSuccess}\n ${jsonEncode(resp.data)}\n ${'=' * 10}');
+      debugPrint(
+          '${'=' * 10}\n${resp}\n ${jsonEncode(resp?.data)}\n ${'=' * 10}');
       expect(
-        resp.isSuccess,
+        resp?.data,
         true,
       );
     });
